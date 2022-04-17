e3aaccdb99c1cea5a1c2acbad63eec7d03d2cccd
bo0tzz <btdewilligen@gmail.com>
2022-04-17 14:15:18 +0200

    Clean up config

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,8 +3,8 @@ import Config
 config :yt_emby_sync, YoutubeDl,
   bin: System.get_env("YTDL_BIN") || System.find_executable("yt-dlp")
 
-config :yt_emby_sync, Files,
-  download_folder: System.get_env("DOWNLOAD_FOLDER") || "/Users/boet/tmp/youtube-download"
+config :yt_emby_sync, Files, download_folder: System.fetch_env!("DOWNLOAD_FOLDER")
 
-config :yt_emby_sync, :targets,
-  youtube_playlist_url: "https://www.youtube.com/playlist?list=PL3g5xwbQbHORmJmVtwiIjGhGs8RVJZptj"
+config :yt_emby_sync, :targets, youtube_playlist_url: System.fetch_env!("YT_PLAYLIST_URL")
+
+config :yt_emby_sync, :destructive, System.get_env("DESTRUCTIVE") != nil
