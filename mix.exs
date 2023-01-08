270576664ea85cc70631b5c39c96048057da42b2
bo0tzz <git@bo0tzz.me>
2023-01-08 20:29:49 +0100

    Version 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YtEmbySync.MixProject do
   def project do
     [
       app: :yt_emby_sync,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
